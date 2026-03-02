e23e3fa8b79b4d09fe5f21eec737ff0c9eef4cf9
Nehir Sarıkaya <nehirsarikaya1907@gmail.com>
2026-03-03 01:40:32 +0300

    Update program to specify for primes.

--- prime.c
+++ prime.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(void){
-  for (int i=1; i <= 50; ++i){ 
+  for (int i=1; i <= 500; ++i){ 
     int flag = 0;
     for (int j=1; j<i; ++j){ 
       if(i%j == 0 && j!=1){
         flag = 1;
       }
     }
-    flag | (i==1) ? printf("%d ben asal degilim.\n", i):printf("%d ben asalım.\n", i);
+    if (!(flag | (i==1))) printf("%d ben asalım.\n", i);
   }
 }
 
